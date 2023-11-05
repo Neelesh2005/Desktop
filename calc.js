832630ae2ba5e55975d51fb14f6734b33ebc84d2
Neelesh2005 <f20230078@goa.bits-pilani.ac.in>
2023-11-05 20:08:08 +0530

    responsive

--- calc.js
+++ calc.js
@@ -12,8 +12,7 @@ const power = document.querySelector(".power");
 
 expand.addEventListener("click", () => {
     if (expand.innerText === "S") {
-        if(window.innerWidth<="576"){expand.innerHTML="S"}
-        else{expand.innerHTML = "M"}
+        expand.innerHTML = "M"
 
     }
     
